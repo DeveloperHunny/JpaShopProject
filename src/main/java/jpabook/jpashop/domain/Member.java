2802e7e51857057d8a51eac84190780b99e503e1
DeveloperHunny <eogns0321@gmail.com>
2022-04-27 20:56:31 +0900

    [add] order api v1,v2,v3 create

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -23,6 +23,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") //연관관계 설정(주인x) 조회 권한만 있고 DB에 영향을 줄 수 없다.
     private List<Order> orders = new ArrayList<>(); //이후에 이 객체를 변경하면 안 됨. hibernates가 이 객체를 다른 클래스로 감싸고 있음. 이 객체를 바꾸면 hibernate가 관리 못 함.
 
